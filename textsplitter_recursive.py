60ec33cc5e2167eb51f59d6686d2a11238f6997b
yuki-zmstr <yukihide99@gmail.com>
2024-08-18 20:33:55 +0900

    Text splitter

---
+++ textsplitter_recursive.py
@@ -0,0 +1,13 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+TEXT = "12345 67890abcdefghij"
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=5,
+    chunk_overlap=2,
+    separators=[" ", ""]
+)
+
+splitted = text_splitter.split_text(TEXT)
+for i in range(len(splitted)):
+    print(i, splitted[i])
